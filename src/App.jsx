7d385132acf754f07c842dccc3ee93757c1f8225
sarveshdevrukhkarftech <sarveshdevrukhkar@fafadiatech.com>
2025-12-05 15:55:48 +0530

    Initial setup of the React-To-To-Do Application.

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <h2>React to To Do App</h2>
+      <h3>Internship Project at Fafadia Tech</h3>
+    </>
+  );
+}
+
+export default App;
