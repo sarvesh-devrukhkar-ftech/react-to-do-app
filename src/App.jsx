ec736ecdc308a19a423da4c0becaf8982d5d62bb
sarvesh.devrukhkar <sarvesh.devrukhkar@fafadiatech.com>
2025-12-17 16:44:05 +0530

    Added basic task functionality.
    Implementation:
    - Task input.
    - Task rendering on Page.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import "./App.css";
 import ToDoApp from "./components/ToDoApp";
 
-function App() {
+export default function App() {
     return (
         <>
             <ToDoApp />
         </>
     );
 }
-
-export default App;
--- src/components/ToDoApp.jsx
+++ src/components/ToDoApp.jsx
@@ -1,79 +1,31 @@
 import "../styles/ToDoApp.css";
-import React, { useState } from "react";
+import { useState } from "react";
 import Task from "./Task";
 
 export default function ToDoApp() {
-    const [tasks, setTasks] = useState([
-        { id: 1, task: "Task-1", completed: false },
-        { id: 2, task: "Task-2", completed: true },
-    ]);
+    const [task, setTask] = useState(""); // Task Input.
+    const [tasks, setTasks] = useState([]); // Task List.
 
-    // const [text, setText] = useState(""); // Hooks for Task Text.
-
-    function addTask(text) {
-        const newTask = {
-            id: Date.now(),
-            text,
-            completed: false,
-        };
-
-        setTasks([...tasks, newTask]); // Creating new array in the state with new task.
-        setText(""); // Setting Input Field Empty.
+    function handleAddTask() {
+        setTasks([...tasks, task]);
+        setTask(""); // clear input
     }
 
     return (
         <div>
             <h1>React to To Do</h1>
             <div className="task-container">
-                {tasks.map((task) => (
-                    <Task
-                        // key={task.id}
-                        // deleteTask={deleteTask}
-                        // toggleCompleted={task.completed}
-                        task={task}
-                    />
-                ))}
+                {tasks.map((task, index) => <Task task={task} />)}
             </div>
+
             <div className="input-container">
-                <label htmlFor="add-task" for="add-task">
-                    Add Task
-                </label>
-                <input type="text" name="add-task" id="add-task" required />
-                <button onClick={() => addTask(text)}>
+                <label htmlFor="add-task" for="add-task">Add Task </label>
+                <input type="text" value={task} placeholder="Add task..."
+                    onChange={(event) => setTask(event.target.value)} required />
+                <button onClick={() => handleAddTask()}>
                     <b>Add</b>
                 </button>
             </div>
         </div>
     );
 }
-
-/*
-function TodoList() {
-
-function deleteTask(id) {
- setTasks(tasks.filter(task => task.id !== id));
- }  
-
- function toggleCompleted(id) {
- setTasks(tasks.map(task => {
- if (task.id === id) {
- return {…task, completed: !task.completed};
- } else {
- return task;
- } 
- }));
- }
-
- return (
- <div className="todo-list">
- <input
- value={text}
- onChange={e => setText(e.target.value)} 
- />
-
- <button onClick={() => addTask(text)}>Add</button>
- </div>
- );
-}
-export default TodoList;
-*/
